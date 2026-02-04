fd442b0383acd11e7e0e2b030f5e0948b809cab2
Joy Ray <joys57725@gmail.com>
2026-02-04 12:07:51 +0600

    released 2.4

--- Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Activity/ui/theme/Color.kt
+++ Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Activity/ui/theme/Color.kt
@@ -10,7 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
 //light colors--------------------------------------------------
 
 val LightStatusBar = Color(0xFF008080)
