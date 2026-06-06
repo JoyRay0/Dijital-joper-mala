c1b82e42844a29b631a66cd9c01889a5295a86ce
Joy Ray <joys57725@gmail.com>
2026-06-06 19:49:20 +0600

    Added sanitize helper function

---
+++ Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Helper/SanitizeHelper.kt
@@ -0,0 +1,21 @@
+package com.mala.digital_joper_mala.Helper
+
+object SanitizeHelper {
+
+    fun sanitizeNumber(number : String) : Long{
+
+        val clean = number.replace(Regex("""\D+"""), "")
+
+        return clean.toLongOrNull() ?: 0L
+
+    }
+
+    fun sanitizeText(text : String) : String{
+
+        val _text = text.replace(Regex("""[\p{L}\p{P}\p{M}]+"""), "")
+
+        return _text
+
+    }
+
+}
