119eb7fc96665490c6bb22d634ee2c01e9adb7f1
Joy Ray <joys57725@gmail.com>
2026-08-10 14:41:28 +0600

    Add : retry helper function to retry api call

--- Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Helper/RetryHelper.kt
+++ Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Helper/RetryHelper.kt
@@ -1,4 +1,54 @@
 package com.mala.digital_joper_mala.Helper
 
-class RetryHelper {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
+
+class RetryHelper(
+    private val scope : CoroutineScope
+) {
+
+    fun retry(
+        maxTime : Long = 60_000,
+        delayTime : Long = 5_000,
+        request : (success : () -> Unit, failed : () -> Unit) -> Unit,
+        onSuccess : () -> Unit,
+        onFailed : () -> Unit
+
+    ){
+
+        val startTime = System.currentTimeMillis()
+
+        fun execute(){
+
+            if (System.currentTimeMillis() - startTime >= maxTime){
+
+                onFailed()
+                return
+
+            }
+
+            request( {
+
+                onSuccess()
+
+            }, {
+
+                scope.launch {
+
+                    delay(delayTime.milliseconds)
+
+                    execute()
+
+                }
+
+            } )
+
+        }
+
+        execute()
+
+    }
+
 }
