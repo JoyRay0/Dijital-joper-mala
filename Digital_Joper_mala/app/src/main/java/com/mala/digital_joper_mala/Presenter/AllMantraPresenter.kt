3d0fc4c6e029baa9540b83f52ed2d1d8da437a86
Joy Ray <joys57725@gmail.com>
2026-08-10 14:42:08 +0600

    Add : retry helper

--- Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Presenter/AllMantraPresenter.kt
+++ Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/Presenter/AllMantraPresenter.kt
@@ -1,18 +1,29 @@
 package com.mala.digital_joper_mala.Presenter
 
 import android.content.Context
+import android.util.Log
+import com.mala.digital_joper_mala.Helper.RetryHelper
+import com.mala.digital_joper_mala.Helper.ShortMessageHelper
 import com.mala.digital_joper_mala.Model.AllMantraModel
+import com.mala.digital_joper_mala.Model.MantraItem
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.time.Duration.Companion.milliseconds
 
 interface AllMantra{
-
     fun dialogStatus(value : Boolean)
-
+    fun allMantraList(list: List<MantraItem>)
+    fun favoriteMantraList(list: List<MantraItem>)
+    fun searchMantraList(list: List<MantraItem>)
+    fun deleteFavoriteMantraStatus(status : String)
 }
 
 class AllMantraPresenter(
@@ -23,6 +34,8 @@ class AllMantraPresenter(
     private val model = AllMantraModel(context)
     private val scopeIO = CoroutineScope(Dispatchers.IO + SupervisorJob())
     private val scopeMain = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
+    private val retry = RetryHelper(scopeIO)
 
 
     fun setAllMantraCache(value : Boolean){
@@ -35,6 +48,51 @@ class AllMantraPresenter(
 
     }
 
+    fun getAllMantraFromServer(){
+
+        retry.retry(
+            maxTime = 60_000,
+            delayTime = 5_000,
+            request = { success , failed ->
+
+                scopeIO.launch {
+
+                    model.getAllMantraFromServer(onSuccess = {
+
+                        if (it){
+
+                            success()
+
+                        }
+
+                    }, onFailed = {
+
+                        failed()
+
+                    })
+
+                }
+
+
+            },
+            onSuccess = {
+
+                val data = model.getAllMantra()
+
+                scopeMain.launch {
+
+                    view.allMantraList(data)
+
+                }
+
+            },
+            onFailed = {}
+        )
+
+
+
+    }
+
     fun getAllMantraCache(){
 
         scopeIO.launch {
@@ -44,6 +102,100 @@ class AllMantraPresenter(
             withContext(Dispatchers.Main){
 
                 view.dialogStatus(data)
+
+            }
+
+        }
+
+    }
+
+    fun searchMantraInAllMantra(title: String){
+
+        scopeIO.launch {
+
+            val data = model.searchAllMantra(title)
+
+            withContext(Dispatchers.Main){
+
+                view.searchMantraList(data)
+
+            }
+
+        }
+
+    }
+
+    fun getAllMantra(){
+
+        scopeIO.launch {
+
+            val data = model.getAllMantra()
+
+            withContext(Dispatchers.Main){
+
+                view.allMantraList(data)
+
+            }
+
+        }
+
+    }
+
+    fun favoriteMantraInsert(title : String, mantra : String){
+
+        scopeIO.launch {
+
+            model.favoriteInsert(title, mantra)
+
+            val data = model.getFavoriteMantra()
+
+            withContext(Dispatchers.Main){
+
+                view.favoriteMantraList(data)
+
+            }
+
+        }
+
+    }
+
+    fun getAllFavoriteMantra(){
+
+        scopeIO.launch {
+
+            val data = model.getFavoriteMantra()
+
+            withContext(Dispatchers.Main){
+
+                view.favoriteMantraList(data)
+
+            }
+
+        }
+
+    }
+
+    fun deleteFavoriteMantra(mantra: String){
+
+        scopeIO.launch {
+
+            val isDeleted = model.deleteFavoriteMantra(mantra)
+
+            val data = model.getFavoriteMantra()
+
+            withContext(Dispatchers.Main){
+
+                if (isDeleted){
+
+                    view.favoriteMantraList(data)
+
+                    view.deleteFavoriteMantraStatus("ডিলিট হয়েছে")
+
+                }else{
+
+                    view.deleteFavoriteMantraStatus("ডিলিট হয়নি")
+
+                }
 
             }
 
