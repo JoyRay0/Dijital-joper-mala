7405afc38598433e5737dc77e42f7ee6f97fff34
Joy Ray <joys57725@gmail.com>
2026-08-15 18:45:32 +0600

    Add : Achievement dialog

--- Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/View/Act_boishnob_mala.kt
+++ Digital_Joper_mala/app/src/main/java/com/mala/digital_joper_mala/View/Act_boishnob_mala.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
@@ -23,9 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -127,6 +130,7 @@ private fun BoisnobMalaFullScreen(
 ) {
 
     var isMantraDialogVisible by remember { mutableStateOf(false) }
+    val countMilestones = listOf(1000L, 5000L, 10000L, 50000L)
 
     Scaffold(
 
@@ -180,13 +184,24 @@ private fun BoisnobMalaFullScreen(
                 MantraDialog(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(color = if (isDark) Color.White.copy(alpha = 0.5f) else Color.Black.copy(alpha = 0.5f)),
+                        .background(
+                            color = if (isDark) Color.White.copy(alpha = 0.5f) else Color.Black.copy(
+                                alpha = 0.5f
+                            )
+                        ),
                     isDark = isDark,
                     mantraList = mantraList,
                     closeClick = { isMantraDialogVisible = false }
                 )
 
             }
+
+            MilestonesDialog(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = if (isDark) Color.White.copy(alpha = 0.5f) else Color.Black.copy(alpha = 0.5f))
+                    .align(Alignment.BottomCenter)
+            )
 
 
         }//box
@@ -472,4 +487,167 @@ private fun Item(
 
     }//box
 
+}//fun end
+
+
+@Preview(showBackground = true)
+@Composable
+private fun MilestonesDialog(
+    modifier: Modifier = Modifier,
+    closeClick: () -> Unit = {},
+    currentCount : Long = 50000L,
+    isDark: Boolean = true
+) {
+
+    val countMilestones = listOf(1000L, 5000L, 10000L, 50000L)
+    val milestonesTitle = listOf(
+        "\uD83C\uDF89 অভিনন্দন!",
+        "\uD83C\uDF38 অসাধারণ সাধনা!",
+        "\uD83C\uDF1F ১০,০০০ জপ সম্পন্ন!",
+        "\uD83C\uDFC6 অসাধারণ অর্জন!"
+    )
+    val milestonesDescription = listOf(
+        "আপনি সফলভাবে ১,০০০ বার জপ সম্পন্ন করেছেন। আপনার এই সাধনা অব্যাহত থাকুক। \uD83D\uDE4F",
+        "আপনার জপের সংখ্যা ৫,০০০ পূর্ণ হয়েছে। নিয়মিত জপের এই সুন্দর অভ্যাস ধরে রাখুন। \uD83D\uDE4F",
+        "আপনার অধ্যবসায় সত্যিই প্রশংসনীয়। ১০,০০০ জপের এই অর্জনের জন্য অভিনন্দন। \uD83D\uDE4F",
+        "আপনি ৫০,০০০ জপ সম্পন্ন করেছেন! আপনার নিষ্ঠা ও সাধনার এই পথ আরও সুন্দর হোক। \uD83D\uDE4F"
+    )
+
+    Box(
+
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(
+                indication = null,
+                interactionSource = null
+            ){}
+            .padding(12.dp)
+
+    ) {
+
+        Column(
+
+            modifier = Modifier
+                .fillMaxWidth()
+                //.shadow(elevation = 3.dp, shape = RoundedCornerShape(14.dp))
+                .clip(shape = RoundedCornerShape(14.dp))
+                .clickable(
+                    indication = null,
+                    interactionSource = null
+                ){}
+                .background(color =if (isDark) Color.Black.copy(alpha = 0.45f) else Color.White.copy(alpha = 0.70f))
+                .border(
+                    width = 1.dp,
+                    shape = RoundedCornerShape(14.dp),
+                    color = if (isDark) Color.White.copy(alpha = 0.6f) else Color.White.copy(alpha = 0.8f)
+                )
+                .padding(7.dp)
+                .align(Alignment.BottomCenter)
+
+        ) {
+
+            Spacer(modifier = Modifier.height(7.dp))
+
+            Box(
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+
+            ) {
+
+                Text(
+                    text = "\uD83C\uDF89 নতুন অর্জন!",
+                    fontSize = 18.sp,
+                    fontFamily = BanglaHelper.banglaFont(),
+                    fontWeight = FontWeight.Bold,
+                    color = if (isDark) Color(0xFFEAEAEA) else Color(0xFF000000),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .align(Alignment.Center)
+
+                )
+
+                /* close button */
+                Box(
+
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .clip(shape = CircleShape)
+                        //.background(color = Color.Gray)
+                        .clickable { closeClick() }
+                        .size(30.dp)
+                        .align(Alignment.CenterEnd)
+
+                ) {
+
+                    Icon( painter = painterResource(com.mala.digital_joper_mala.R.drawable.ic_wrong),
+                        contentDescription = "",
+                        tint = if (isDark) Color(0xFFFFFFFF) else Color(0xFF000000),
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .size(18.dp)
+                            .align(Alignment.Center)
+
+                    )
+
+                }
+
+            }//box
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Text(
+                text = when(currentCount){
+
+                    countMilestones[0] -> milestonesTitle[0]
+                    countMilestones[1] -> milestonesTitle[1]
+                    countMilestones[2] -> milestonesTitle[2]
+                    countMilestones[3] -> milestonesTitle[3]
+                    else -> ""
+
+                },
+                fontSize = 16.sp,
+                fontFamily = BanglaHelper.banglaFont(),
+                fontWeight = FontWeight.SemiBold,
+                color = if (isDark) Color(0xFFFFFFFF) else Color(0xFF000000),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+                    .align(Alignment.CenterHorizontally)
+
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = when(currentCount){
+
+                    countMilestones[0] -> milestonesDescription[0]
+                    countMilestones[1] -> milestonesDescription[1]
+                    countMilestones[2] -> milestonesDescription[2]
+                    countMilestones[3] -> milestonesDescription[3]
+                    else -> ""
+
+                },
+                fontSize = 16.sp,
+                fontFamily = BanglaHelper.banglaFont(),
+                fontWeight = FontWeight.Normal,
+                color = if (isDark) Color(0xFFE3E2E2) else Color(0xFF000000),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+                    .align(Alignment.CenterHorizontally)
+
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+        }//column
+
+    }//box
+    
 }//fun end
